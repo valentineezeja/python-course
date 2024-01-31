8d8306de63d62a9d7c12808ffa5f2c3db6906afe
Valentine <Valentine>
2024-01-31 22:43:42 +0000

    Added v3 played by computer

--- rps-v3.py
+++ rps-v3.py
@@ -1,22 +1,30 @@
-player1 = input("Enter player1's entry ")
-player2 = input("Enter player 2's entry ")
+import random    #you can use from random import radint; that way you only need to use radint(....) for your main command
+# player1 = input("Enter player1's entry ")
+player2 = input("Enter player 2's entry ").lower()
+auto = random.randint(1,3)
+if auto == 1:
+    computer = "rock"
+elif auto == 2:
+    computer = "paper"
+elif auto == 3:
+    computer = "scissors"
+print(f"computer played {computer}")
 
-# logic here
-if player1 == player2:
+if computer == player2:
     print("it's a tie")
-elif player1 == "rock":
+elif computer == "rock":
     if player2 == "paper":
-        print("player1 wins")
+        print("player2 wins")
     elif player2 == "scissors":
-        print("player1 wins")
-elif player1 == "scissors":
+        print("computer wins")
+elif computer == "scissors":
     if player2 == "paper":
-        print("player1 wins")
+        print("computer wins")
     elif player2 == "rock":
         print("player2 wins")
-elif player1 == "paper":
+elif computer == "paper":
     if player2 == "rock":
-        print("player1 wins")
+        print("computer wins")
     elif player2 == "scissors":
         print("player2 wins")
 else:
