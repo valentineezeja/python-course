b3b07799096d4f0b05fe302c498dc22465c3286e
Valentine <Valentine>
2024-02-03 00:58:11 +0000

    added more files

---
+++ lists/list-loop.py
@@ -0,0 +1,8 @@
+#joins all the items in the list
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Define your code below:
+result = ""
+for item in sounds:
+    result += item.upper()
+    print(result)
