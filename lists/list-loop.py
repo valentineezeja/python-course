e737017cafb018373a674d2b1974088d993d1bb6
Valentine <valentineezeja@gmail.com>
2024-02-10 23:58:06 +0000

    started afresh

--- lists/list-loop.py
+++
@@ -1,8 +0,0 @@
-#joins all the items in the list
-sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
-
-# Define your code below:
-result = ""
-for item in sounds:
-    result += item.upper()
-    print(result)
