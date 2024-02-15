040d4296039b4220eb024358df378d3d2d892be6
Valentine <valentineezeja@gmail.com>
2024-02-15 01:04:15 +0000

    updated **kwargs

--- 07.functions-2/Notes.md
+++ 07.functions-2/Notes.md
@@ -1,2 +1,21 @@
-**args: creates a tuple
-*kwargs: creates a dictionary
+
+**args: gathers remaining arguments into a tuple
+*kwargs: gathers remaining arguments into a dictionary
+
+#Parameter ordering in a function:
+1. parameter
+2. *args
+3. Default parameters
+4. **kwargs
+
+# Tuple unpacking:
+Because *args expect a tuple input, if a list is passed to the function for execution,
+it treats it as 1 entity and the required job fails. To convert (unpack) the provided
+argument into a tuple in order to be seen as separate values by python,
+use *
+Example: my_list = [1, 2, 3, 4, 5]
+print my_function ((*my_list))
+
+# Dictionary unpacking:
+Works same as Tuple unpacking but for dictionaries.
+See example in files.
--- 07.functions-2/kwargs.py
+++ 07.functions-2/kwargs.py
@@ -15,15 +15,29 @@
 #
 # print(my_colour(obi="red", juke="black", ada="green"))
 
-def my_new_func(**kwargs):
-    if "valentine" in kwargs and kwargs["valentine"] == "good":
-        return "You're a good boy"
-    elif "valentine" in kwargs:
-        return f"{kwargs['valentine']} Valentine!!"
-    return "Sorry, I don't know you"
+# def my_new_func(**kwargs):
+#     if "valentine" in kwargs and kwargs["valentine"] == "good":
+#         return "You're a good boy"
+#     elif "valentine" in kwargs:
+#         return f"{kwargs['valentine']} Valentine!!"
+#     return "Sorry, I don't know you"
+#
+#
+# print(my_new_func(David = "hello"))
+# print(my_new_func(valentine= "good"))
+# print(my_new_func(valentine= "bad"))
+
+# Exercise...
+# Define combine_words below:
+def combine_words(word, **kwargs):
+    if "prefix" in kwargs:
+        return f"{kwargs['prefix']}{word}"  # you can also use return here without f"
+    elif "suffix" in kwargs:
+        print(f"{word}{kwargs['suffix']}")  # you can also use return here without f"
+    else:
+        return word
 
 
-print(my_new_func(David = "hello"))
-print(my_new_func(valentine= "good"))
-print(my_new_func(valentine= "bad"))
-
+print(combine_words(word="child", prefix="man"))
+print(combine_words(word="child", suffix="boo"))
+print(combine_words(good="doo", tree="loo"))
