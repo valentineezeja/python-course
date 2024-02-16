93bc316bc1b3ab3c6d289f5334da58fa980e9e3e
Valentine <valentineezeja@gmail.com>
2024-02-16 23:54:37 +0000

    added Lambda

---
+++ 08.lambdas-bultin-functions/Notes.md
@@ -0,0 +1,1 @@
+Lambdas are functions that gets used once - hey are usually inline
---
+++ 08.lambdas-bultin-functions/lambdas.py
@@ -0,0 +1,11 @@
+# cube = lambda num: num ** 3
+# print(cube(2))
+
+
+## maps
+
+nums = [1, 2, 3, 4]
+decrement_list = list(map(lambda: nums -1, nums))
+
+
+print (decrement_list)
