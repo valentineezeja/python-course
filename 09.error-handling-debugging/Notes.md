f14e8b7c4e77dc6cb70c18eeb45f2df11432344d
Valentine <valentineezeja@gmail.com>
2024-02-18 23:39:40 +0000

    completed err-handling and debugging module

---
+++ 09.error-handling-debugging/Notes.md
@@ -0,0 +1,8 @@
+# Errors:
+- Syntax error
+- Name error
+- Type error
+- Index error
+- Value error
+- Key error
+- Attribute error
---
+++ 09.error-handling-debugging/exrcise.py
@@ -0,0 +1,27 @@
+# def divide(num1, num2):
+#     result = 0
+#     try:
+#         result = num1 / num2
+#     except TypeError as err:
+#         print("Please provide two integers or floats")
+#     except ZeroDivisionError as err:
+#         print("Please do not divide by zero")
+#     finally:
+#         return result
+#
+#
+# divide(1, 0)
+
+# Using raise
+def divide(num1, num2):
+    try:
+        result = num1 / num2
+    except TypeError as err:
+        print("Please provide two integers or floats")
+    except ZeroDivisionError as err:
+        print("Please do not divide by zero")
+    finally:
+        return result
+
+
+divide(1, 0)
