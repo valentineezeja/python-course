7983245f05269b7b3b3545e895deec8189f96428
Valentine <valentineezeja@gmail.com>
2024-02-20 23:46:41 +0000

    api update

--- 11.http-requests/your-frst-python-request.py
+++ 11.http-requests/your-frst-python-request.py
@@ -3,3 +3,9 @@ url = "http://hackernews.com"
 request = requests.get(url)
 
 print(request.headers)
+status = None
+if request.status_code == 200:
+    status = "Successful"
+else:
+    status = "Failed"
+print(f"The status of your request to {url} was '{status}' with code {request.status_code}")
