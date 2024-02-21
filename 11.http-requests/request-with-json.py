ff12685e48269da655363a31aaff6cc99e90f8b5
Valentine <valentineezeja@gmail.com>
2024-02-21 00:02:16 +0000

    api update-3

--- 11.http-requests/request-with-json.py
+++ 11.http-requests/request-with-json.py
@@ -1,17 +1,17 @@
 import requests
 url = "https://icanhazdadjoke.com/"
 
-plain_ressponse = response = requests.get(url, headers={"Accept": "text/plain"})
+# plain_ressponse = response = requests.get(url, headers={"Accept": "text/plain"})
 response = requests.get(url, headers={"Accept": "application/json"})
 
-print(plain_ressponse.text)
+# print(plain_ressponse.text)
 
-print(response)
+# print(response.json())
 
-# data = response.json()
-#
-# print(data["joke"])
-# print(f"status: {data['status']}")
+data = response.json()
+
+print(data["joke"])
+print(f"status: {data['status']}")
 
 
 
