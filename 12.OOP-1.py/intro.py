37268720e5d9b2c91777287a16b2e5c1a112c826
Valentine <valentineezeja@gmail.com>
2024-02-22 00:08:42 +0000

    class intro

--- 12.OOP-1.py/intro.py
+++ 12.OOP-1.py/intro.py
@@ -1,8 +1,24 @@
-class user:
-    pass
+# class User:
+#     pass
+#
+#
+# user1 = User()
+# print(user1)
+# print(type(user1))
+# print(type(User))
+
+class User:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
 
 
-user1 = user()
-print(user1)
-print(type(user1))
-print(type(user))
+user1 = User("Val", "Ez", 31)
+print(user1.first, user1.last, user1.age)
+
+user2 = User("James", "Bloom", 40)
+print(user2.first, user2.last, user2.age)
+
+user3 = User("John", "Bosco", 55)
+print(user3.first, user2.last, user2.age)
